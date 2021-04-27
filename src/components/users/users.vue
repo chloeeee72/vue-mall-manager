2583cc9ff85b64a5a5d972547fb647a76e2af147
chloe <1935513424a@gmaiil.com>
2021-04-27 21:42:21 +0800

    add userpage pressed btn show dialog

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -8,22 +8,33 @@
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
 
-    <!-- 搜索框 -->
+    <!-- 搜索 添加-->
     <el-row class="search-row">
       <el-input
+        clearable
         placeholder="请输入内容"
         v-model="query"
         class="input-with-select"
+        @clear="loadUserList()"
       >
-        <el-button slot="append" icon="el-icon-search"></el-button>
+        <el-button
+          slot="append"
+          @click="search()"
+          icon="el-icon-search"
+        ></el-button>
       </el-input>
-      <el-button class="add-btn" @click="add()" type="success"
-        >成功按钮</el-button
+      <el-button
+        class="add-btn"
+        plain
+        type="success"
+        @click="showAddUserDialog()"
+        >添加按钮</el-button
       >
     </el-row>
 
     <!-- 表格 -->
-    <el-table :data="tableData" style="width: 100%">
+    <!-- :data="userListData":给表格传数据 -->
+    <el-table :data="userListData" style="width: 100%">
       <el-table-column label="#" width="100" type="index"> </el-table-column>
       <el-table-column prop="username" label="姓名" width="100">
       </el-table-column>
@@ -82,12 +93,40 @@
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :current-page="pagenum"
-      :page-sizes="[2, 10, 20, 50]"
+      :page-sizes="[2, 5, 10, 20]"
       :page-size="pagesize"
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
     >
     </el-pagination>
+
+    <!-- 对话框 -->
+    <!-- 添加用户的对话框 -->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+      <el-form :model="form">
+        <el-form-item label="用户名" :label-width="formLabelWidth">
+          <el-input v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item label="密 码" :label-width="formLabelWidth">
+          <el-input v-model="form.password" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item label="邮 箱" :label-width="formLabelWidth">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item label="电 话" :label-width="formLabelWidth">
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisibleAdd = false && add()"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -98,6 +137,7 @@ export default {
   },
   data() {
     return {
+      // 输入框内容
       query: "",
       // 分页相关的数据
       // 总数据条数
@@ -107,10 +147,30 @@ export default {
       // 页总数
       pagesize: 2,
       // 表格绑定的数据
-      tableData: []
+      userListData: [],
+
+      // 添加对话框的属性
+      dialogFormVisibleAdd: false,
+      // 添加用户的表单数据
+      // | username | 用户名称 | 不能为空 |
+      // | password | 用户密码 | 不能为空 |
+      // | email    | 邮箱     | 可以为空 |
+      // | mobile   | 手机号   | 可以为空 |
+      form: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      },
+      // 对话框表单width
+      formLabelWidth: "100px"
     };
   },
   methods: {
+    // 获取数据方法
+    // 1、query=""：获取全部数据
+    // 2、（用于用户搜索）query有值时：根据query显示数据信息，当前页码，每页显示条数
+
     // query 查询参数 null
     // pagenum 当前页码 notnull
     // pagesize 每页显示条数 notnull
@@ -133,7 +193,7 @@ export default {
       } = res.data;
       if (status === 200) {
         // 给表格数据赋值
-        this.tableData = users;
+        this.userListData = users;
         // 给total 赋值
         this.total = total;
         // 提示
@@ -150,7 +210,26 @@ export default {
       this.getUserList();
     },
     handleCurrentChange(val) {
+      this.pagenum = val;
       console.log(`当前页: ${val}`);
+      this.getUserList();
+    },
+
+    // 搜索方法
+    search() {
+      // 直接调用getUserList()方法，根据query的值获取数据
+      // 之后重新渲染页面
+      this.getUserList();
+    },
+
+    // 点击清除输入框，重新获取数据渲染页面
+    loadUserList() {
+      this.getUserList();
+    },
+
+    // 展示添加用户对话框的方法
+    showAddUserDialog() {
+      this.dialogFormVisibleAdd = true;
     }
   }
 };
