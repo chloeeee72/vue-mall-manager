31a51bd0592041e441c0e038a378e0b0f64a120d
chloe <1935513424a@gmaiil.com>
2021-05-12 20:09:40 +0800

    add arrdata

--- src/components/goods/cateParams.vue
+++ src/components/goods/cateParams.vue
@@ -27,6 +27,79 @@
       </el-form-item>
 
       <!-- 遍历渲染动态 静态数据 -->
+      <el-tabs v-model="active" @tab-click="handleClick">
+        <el-tab-pane label="动态参数" name="1">
+          <!-- 按钮 -->
+          <el-button type="danger">设置动态参数</el-button>
+          <!-- 表格 -->
+          <el-table :data="arrDyparams" style="width: 100%">
+            <!-- 展开行 -->
+            <el-table-column type="expand" label="#">
+              <template slot-scope="scope">
+                <!-- 展开行内tag -->
+                <el-tag
+                  v-for="(tag, index) in scope.row.attr_vals"
+                  :key="index"
+                  closable
+                  :disable-transitions="false"
+                  @close="
+                    handleClose(
+                      scope.row.attr_vals,
+                      scope.row.attr_id,
+                      scope.row.attr_name,
+                      tag
+                    )
+                  "
+                >
+                  {{ tag }}</el-tag
+                >
+                <el-input
+                  class="input-new-tag"
+                  v-if="inputVisible"
+                  v-model="inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm(scope.row.attr_vals)"
+                  @blur="handleInputConfirm(scope.row.attr_vals)"
+                >
+                </el-input>
+                <el-button
+                  v-else
+                  class="button-new-tag"
+                  size="small"
+                  @click="showInput"
+                  >+ New Tag</el-button
+                >
+              </template>
+            </el-table-column>
+            <!-- 属性名称 -->
+            <el-table-column label="属性名称" prop="attr_name">
+            </el-table-column>
+            <!-- 操作按钮 -->
+            <el-table-column label="操作" mini>
+              <template>
+                <el-button
+                  type="primary"
+                  plain
+                  icon="el-icon-edit"
+                  size="mini"
+                  circle
+                ></el-button>
+
+                <!-- 删除按钮 -->
+                <el-button
+                  type="danger"
+                  plain
+                  icon="el-icon-delete"
+                  size="mini"
+                  circle
+                ></el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="静态参数" name="2">静态参数</el-tab-pane>
+      </el-tabs>
     </el-form>
   </el-card>
 </template>
@@ -39,7 +112,7 @@ export default {
       // 数据表单
       form: {},
       // 级联选择器
-      selectedOptions: [1, 3, 6],
+      selectedOptions: [],
       options: [],
       defalutProp: {
         label: "cat_name",
@@ -49,7 +122,11 @@ export default {
       // 动态参数的数据数组
       arrDyparams: [],
       // 静态参数的数据数组
-      arrStaticparams: []
+      arrStaticparams: [],
+      active: "1",
+      // 展开行数据
+      inputVisible: false,
+      inputValue: ""
     };
   },
   created() {
@@ -62,6 +139,7 @@ export default {
       // console.log(res);
       this.options = res.data.data;
     },
+
     // 级联选择器 @change 当选中节点变化时触发 选中节点的值
     async handleChange() {
       if (this.selectedOptions.length !== 3) {
@@ -81,6 +159,7 @@ export default {
           item.attr_vals =
             item.attr_vals.length == 0 ? [] : item.attr_vals.trim().split(",");
         });
+        console.log(this.arrDyparams);
 
         // 获取静态数据
         //   if (this.selectedOptions.length !== 3) {
@@ -93,6 +172,50 @@ export default {
         //   );
         //   this.arrStaticparams = res.data.data;
       }
+    },
+
+    // tabs切换时触发
+    handleClick() {},
+
+    // 展开行内tag
+    // 点击x按钮
+    // - 请求路径：categories/:id/attributes/:attrId
+    // - 请求方法：put
+    async handleClose(attr_vals, attr_id, attr_name, tag) {
+      // attr_vals, attr_id, attr_name,tag
+      attr_vals.splice(attr_vals.indexOf(tag), 1);
+      // 发送请求
+      // | :id       | 分类 ID                | 不能为空`携带在url中`      |
+      // | :attrId   | 属性 ID                | 不能为空`携带在url中`      |
+      // | attr_name | 新属性的名字           | 不能为空，携带在`请求体`中 |
+      // | attr_sel  | 属性的类型[many或only] | 不能为空，携带在`请求体`中 |
+      // | attr_vals | 参数的属性值           | 可选参数，携带在`请求体`中 |
+      let putData = {
+        attr_name: attr_name,
+        attr_sel: "many",
+        attr_vals: attr_vals.join(",")
+      };
+      const res = await this.$http.put(
+        `categories/${this.selectedOptions[2]}/attributes/${attr_id}`,
+        putData
+      );
+      console.log(res);
+    },
+    // 点击 +newtag 按钮
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
+    // 回车键 or 失去焦点
+    handleInputConfirm(attr_vals) {
+      let inputValue = this.inputValue;
+      if (inputValue) {
+        attr_vals.push(inputValue);
+      }
+      this.inputVisible = false;
+      this.inputValue = "";
     }
   }
 };
@@ -102,5 +225,20 @@ export default {
 .alert {
   margin-top: 25px;
   margin-bottom: 25px;
+}
+.el-tag + .el-tag {
+  margin-left: 10px;
+}
+.button-new-tag {
+  margin-left: 10px;
+  height: 32px;
+  line-height: 30px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.input-new-tag {
+  width: 90px;
+  margin-left: 10px;
+  vertical-align: bottom;
 }
 </style>
