0e2db7ab1bb4598f8a2c5913c8c0332d0cda0c06
chloe <1935513424a@gmaiil.com>
2021-05-14 11:45:57 +0800

    add goodsCategories add cate btn

--- src/components/goods/goodsCategories.vue
+++ src/components/goods/goodsCategories.vue
@@ -17,8 +17,8 @@
         <!-- 级联选择器 -->
         <el-form-item label="分类" :label-width="formLabelWidth">
           <el-cascader
-            :show-all-levels="false"
             props.expandTrigger="hover"
+            :show-all-levels="false"
             v-model="selectedOptions"
             :options="options"
             @change="handleChange"
@@ -32,8 +32,17 @@
         <el-button type="primary" @click="addCate()">确定</el-button>
       </div>
     </el-dialog>
+
     <!-- 表格 -->
-    <el-table height="480" :data="list" style="width:100%">
+    <el-table
+      height="480"
+      :data="list"
+      style="width:100%"
+      row-key="cat_id"
+      :tree-props="{ children: 'children' }"
+    >
+      <el-table-column label="分类名称" prop="cat_name"> </el-table-column>
+
       <el-table-column label="级别">
         <template slot-scope="scope">
           <span v-if="scope.row.cat_level === 0">一级</span>
@@ -113,36 +122,67 @@ export default {
     // 获取数据
     // 列表数据
     this.getGoodsCate();
-    // 三级分类下拉选项数据
-    this.getGoods();
   },
   methods: {
     // 添加分类按钮
     // 显示对话框
     async addGoodsCate() {
+      // 获取所有二级分类数据 - 添加分类 - 下拉列表
+      const res = await this.$http.get(`categories?type=2`);
+      this.options = res.data.data;
       this.dialogFormVisibleAdd = true;
     },
-    // 发送请求
-    async addCate() {},
-
-    // 获取三级分类数据 - 添加分类 - 下拉列表
-    async getGoods() {
-      const res = await this.$http.get(`categories?type=3`);
-      // console.log(res);
-      this.options = res.data.data;
+    // 对话框 - 确定按钮发送请求
+    async addCate() {
+      // - 请求路径：categories
+      // - 请求方法：post
+      // - 请求参数
+      // | 参数名    | 参数说明  | 备注                                                        |
+      // | --------- | --------- | ----------------------------------------------------------- |
+      // | cat_pid   | 分类父 ID | 不能为空，如果要添加1级分类，则父分类Id应该设置为  `0`      |
+      // | cat_name  | 分类名称  | 不能为空                                                    |
+      // | cat_level | 分类层级  | 不能为空，`0`表示一级分类；`1`表示二级分类；`2`表示三级分类 |
+      //  一级分类 selectedOptions.length==0 cat_pid=0 cat_level=0
+      //  二级分类 selectedOptions.length==1 cat_pid=selectedOptions[0] cat_level=1
+      //  三级分类 selectedOptions.length==2 cat_pid=selectedOptions[1] cat_level=2
+      if (this.selectedOptions.length === 0) {
+        this.form.cat_pid = 0;
+        this.form.cat_level = 0;
+      } else if (this.selectedOptions.length === 1) {
+        this.form.cat_pid = this.selectedOptions[0];
+        this.form.cat_level = 1;
+      } else if (this.selectedOptions.length === 2) {
+        this.form.cat_pid = this.selectedOptions[1];
+        this.form.cat_level = 2;
+      }
+      const res = await this.$http.post(`categories`, this.form);
+      console.log(res);
+      if (res.data.meta.status === 201) {
+        // 清空数据
+        (this.form = {
+          cat_pid: -1,
+          cat_name: "",
+          cat_level: -1
+        }),
+          (this.selectedOptions = [])(
+            // 关闭对话框
+            (this.dialogFormVisibleAdd = false)
+          );
+        // 提示消息
+        this.$message.success(res.data.meta.msg);
+      } else {
+        this.$message.warning(res.data.meta.msg);
+      }
     },
 
-    async handleChange() {
-      console.log(this.selectedOptions);
-      console.log(this.options);
-    },
+    async handleChange() {},
     // 获取列表数据
     async getGoodsCate() {
       const res = await this.$http.get(
         `categories?type=3&pagenum=${this.pagenum}&pagesize=${this.pagesize}`
       );
       this.list = res.data.data.result;
-      console.log(this.list);
+      // console.log(this.list);
       this.total = res.data.data.total;
     },
     // 分页方法
